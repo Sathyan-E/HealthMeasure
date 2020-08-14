abb3d91d8b5d19789440aa53363884b69c89f548
Sathyan Elangovan <sathyan576@gmail.com>
2020-08-14 19:59:15 +0530

    MVVM implemented

--- app/src/main/java/com/healthykid/healthbasicmeasure/view/MainActivity.kt
+++ app/src/main/java/com/healthykid/healthbasicmeasure/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.healthykid.healthbasicmeasure
+package com.healthykid.healthbasicmeasure.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.healthykid.healthbasicmeasure.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
