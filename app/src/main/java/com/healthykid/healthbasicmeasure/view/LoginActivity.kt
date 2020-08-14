e2999a9bfe9c80803451bcb0bdacf41177fb309d
Sathyan Elangovan <sathyan576@gmail.com>
2020-08-14 20:19:49 +0530

    Firebase is connected to the project.

--- app/src/main/java/com/healthykid/healthbasicmeasure/view/LoginActivity.kt
+++ app/src/main/java/com/healthykid/healthbasicmeasure/view/LoginActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.healthykid.healthbasicmeasure.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
