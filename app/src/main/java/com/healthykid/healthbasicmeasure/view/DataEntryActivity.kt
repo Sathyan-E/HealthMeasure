cc3104bcb918ab98187c79c0afd81c6e345ce39d
Sathyan Elangovan <sathyan576@gmail.com>
2020-08-16 16:11:26 +0530

    UI flow is done

--- app/src/main/java/com/healthykid/healthbasicmeasure/view/DataEntryActivity.kt
+++ app/src/main/java/com/healthykid/healthbasicmeasure/view/DataEntryActivity.kt
@@ -1,12 +1,19 @@
 package com.healthykid.healthbasicmeasure.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.healthykid.healthbasicmeasure.R
+import kotlinx.android.synthetic.main.activity_data_entry.*
 
 class DataEntryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_data_entry)
+
+        upload_details_btn.setOnClickListener {
+            val i=Intent(this,ResultActivity::class.java)
+            startActivity(i)
+        }
     }
 }
